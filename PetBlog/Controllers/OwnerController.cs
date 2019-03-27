912ec341d4bf89e701fd21a27c189727da44b3ba
Pawan Joshi <pawanjoshi77@gmail.com>
2019-03-26 21:32:13 -0400

    created owner controller and view for index action

---
+++ PetBlog/Controllers/OwnerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetBlog.Data;
+using PetBlog.Models;
+
+namespace PetBlog.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private readonly PetsBlogContext db;
+        private readonly IHostingEnvironment _env;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private async Task<ApplicationUser> GetCurrentUserAync() => await _userManager.GetUserAsync(HttpContext.User);
+
+        public OwnerController(PetsBlogContext context, IHostingEnvironment env, UserManager<ApplicationUser> usermanager)
+        {
+            db = context;
+            _env = env;
+            _userManager = usermanager;
+        }
+
+        //GET: Owners
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetCurrentUserAync();
+            if(user != null)
+            {
+                if (user.OwnerID == null) { ViewData["UserHasOwner"] = "False"; }
+                else { ViewData["UserHasOwner"] = user.OwnerID.ToString(); }
+                return View(await db.Owners.ToListAsync());
+            }
+            else
+            {
+                ViewData["UserhasOwner"] = "None";
+                return View(await db.Owners.ToListAsync());
+            }
+        }
+    }
+}
--- PetBlog/Startup.cs
+++ PetBlog/Startup.cs
@@ -26,8 +26,8 @@ namespace PetBlog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<PetsBlogContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PetBlogConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
