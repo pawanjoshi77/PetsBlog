7e5251da137d1c8850807be79c87d0895179e640
Pawan Joshi <pawanjoshi77@gmail.com>
2019-03-26 23:39:56 -0400

    added create method and create view

--- PetBlog/Controllers/OwnerController.cs
+++ PetBlog/Controllers/OwnerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -31,7 +32,7 @@ namespace PetBlog.Controllers
         public async Task<ActionResult> Index()
         {
             var user = await GetCurrentUserAync();
-            if(user != null)
+            if (user != null)
             {
                 if (user.OwnerID == null) { ViewData["UserHasOwner"] = "False"; }
                 else { ViewData["UserHasOwner"] = user.OwnerID.ToString(); }
@@ -41,6 +42,61 @@ namespace PetBlog.Controllers
             {
                 ViewData["UserhasOwner"] = "None";
                 return View(await db.Owners.ToListAsync());
+            }
+        }
+        //Get Owner/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind("OwnerName", "OwnerAddress", "MemberSince")] Owner owner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Owners.Add(owner);
+                db.SaveChanges();
+                var res = await MapUserToOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        private async Task<ActionResult> MapUserToOwner(Owner owner)
+        {
+            var user = await GetCurrentUserAync();
+            user.owner = owner;
+            var user_res = await _userManager.UpdateAsync(user);
+            if(user_res == IdentityResult.Success)
+            {
+                Debug.WriteLine("We mapped the owner to the user");
+            }
+            else
+            {
+                Debug.WriteLine("Could not map owner to the user");
+                return BadRequest(user_res);
+            }
+            owner.user = user;
+            owner.UserID = user.Id;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(owner).State = EntityState.Modified;
+                var owner_res = await db.SaveChangesAsync();
+                if (owner_res > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest(owner_res);
+                }
+                
             }
         }
     }
