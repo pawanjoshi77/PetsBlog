f5d186346e6a004302b4c8e52e0dc943c1f77bcd
admbjr <36055206+admbjr@users.noreply.github.com>
2019-04-12 12:59:57 -0400

    species pet view created

--- PetBlog/Controllers/PetController.cs
+++ PetBlog/Controllers/PetController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetBlog.Models;
+using PetBlog.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 
 namespace PetBlog.Controllers
 {
     public class PetController : Controller
     {
+        private readonly PetsBlogContext db;
+        private readonly IHostingEnvironment _env;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private async Task<ApplicationUser> GetCurrentUserAync() => await _userManager.GetUserAsync(HttpContext.User);
+
         public IActionResult Index()
         {
             return View();
